8281d2bc1d327d0832d2eb0974a7c5e54c52b57d
sidharthkk <96009053+sidharthkk@users.noreply.github.com>
2026-06-28 21:15:49 +0530

    added the file for code deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+echo
+docker pull sidharth4589/simple-python-flask-app
+
+# Run the Docker image as a container
+echo
+docker run -d -p 5000:5000 sidharth4589/simple-python-flask-app
